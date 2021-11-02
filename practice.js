0f489b1e032753b06de4a94fac320406b76dfdbe
therobgeorge <georg1rd@gmail.com>
2021-11-01 22:25:34 -0400

    completes less than 100 for ruby and js

--- practice.js
+++ practice.js
@@ -9,12 +9,34 @@
 
 // Explanation: (1 + 2 + 3 + 4) = 10
 
-function sum(numbers) {
-  let sum = 0;
-  numbers.forEach(number => sum = sum + number);
-  return sum;
+// function sum(numbers) {
+//   let sum = 0;
+//   numbers.forEach(number => sum = sum + number);
+//   return sum;
   
 
+// }
+
+// console.log(sum([1, 2, 3, 4]));
+
+// Less than 100 ****************
+// # Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+// # Input: [99, 101, 88, 4, 2000, 50]
+// # Output: [99, 88, 4, 50]
+
+function lessThan(numbers) {
+  let less = [];
+  let index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] < 100) {
+      less.push(numbers[index]);
+      index = index + 1;
+    } else {
+      index = index + 1; 
+    }
+  }
+  return less;
 }
 
-console.log(sum([1, 2, 3, 4]));
+console.log(lessThan([99, 101, 88, 4, 2000, 50]));
--- practice.rb
+++ practice.rb
@@ -12,12 +12,34 @@
 
 
 
-def sum(numbers)
-  sum = 0
+# def sum(numbers)
+#   sum = 0
+#   numbers.each do |number|
+#     sum = sum + number
+#   end
+#   sum
+# end
+
+# p sum([1, 2, 3, 4])
+
+
+
+
+# Less than 100 *********************
+
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+def less_than(numbers)
+  less = []
   numbers.each do |number|
-    sum = sum + number
+    if number < 100
+      less << number
+    end
   end
-  sum
+  less
 end
 
-p sum([1, 2, 3, 4])
+p less_than([99, 101, 88, 4, 2000, 50])
