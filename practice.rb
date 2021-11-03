24f4f7cc49c6e4c6b988534f8e37e93720357446
therobgeorge <georg1rd@gmail.com>
2021-11-03 16:26:56 -0400

    completes skip it for ruby and js

--- practice.rb
+++ practice.rb
@@ -119,3 +119,28 @@
 
 
 # p reverse([1, 2, 3, 4])
+
+# skip it ************************
+
+# Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
+
+# The new array should always start with the first number from the original array. The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
+
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+# Output:
+# [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+def skip(numbers)
+  skipped = []
+  index = 0
+  while index < numbers.length
+    skipped << numbers[index]
+    index = index + numbers[index]
+  end
+  skipped
+end
+
+
+p skip([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
