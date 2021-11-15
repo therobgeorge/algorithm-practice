c4717d93152b260edda6c6fa2f5ccbf7bcac5016
therobgeorge <georg1rd@gmail.com>
2021-11-15 18:41:43 -0500

    completes string difference for ruby and js

--- practice.rb
+++ practice.rb
@@ -270,3 +270,33 @@
 
 # p palindrome("racecar")
 # p palindrome("baloney")
+
+
+# hamming *******************
+
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+# def difference(string1, string2)
+#   index = 0
+#   count = 0
+#   while index < string1.length
+#     if string1[index] != string2[index]
+#       count += 1
+#       index += 1
+#     else 
+#       index += 1
+#     end
+#   end
+#   count
+# end
+
+# p difference("ABCDEFG", "ABCXEOG")
+# p difference("ABCDEFG", "ABCDEFG")
