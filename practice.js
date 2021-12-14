09c4501ca0ad209816540140e8aeb255657f0abb
therobgeorge <georg1rd@gmail.com>
2021-12-14 03:26:42 -0500

    completes complete data 2 for ruby and js

--- practice.js
+++ practice.js
@@ -1049,12 +1049,70 @@
 // Input: {"a" => 1, "b" => 2, "c" => 3}
 // Output: {1 => "a", 2 => "b", 3 => "c"}
 
-function flip(hash) {
-  let flipped = {};
-  for (const [letter, number] of Object.entries(hash)) {
-    flipped[number] = letter;
-  }
-  return flipped;
+// function flip(hash) {
+//   let flipped = {};
+//   for (const [letter, number] of Object.entries(hash)) {
+//     flipped[number] = letter;
+//   }
+//   return flipped;
+// }
+
+// console.log(flip({"a": 1, "b": 2, "c": 3}));
+
+
+// Complete Data 2 ***************
+
+// Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+// For example, given this array of posts (note that the submitted_by is an id number):
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+// ]
+
+// And this array of users:
+
+// [
+// {user_id: 403, name: "Aunty Em"},
+// {user_id: 231, name: "Joelle P."},
+// {user_id: 989, name: "Lyndon Johnson"},
+// {user_id: 111, name: "Patti Q."},
+// ]
+
+// Return the array of posts as follows:
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+function user(posts, users) {
+  let submittedBy = "submitted_by";
+  let userId = "user_id";
+  let name = "name";
+  posts.forEach(post => {
+    users.forEach(user => {
+      if (post[submittedBy] === user[userId]) {
+        post[submittedBy] = user[name];
+      }
+    });
+  });
+  return posts;
 }
 
-console.log(flip({"a": 1, "b": 2, "c": 3}));
+
+console.log(user([
+  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+], [
+  {user_id: 403, name: "Aunty Em"},
+  {user_id: 231, name: "Joelle P."},
+  {user_id: 989, name: "Lyndon Johnson"},
+  {user_id: 111, name: "Patti Q."}]));
--- practice.rb
+++ practice.rb
@@ -1053,12 +1053,67 @@
 # Input: {"a" => 1, "b" => 2, "c" => 3}
 # Output: {1 => "a", 2 => "b", 3 => "c"}
 
-def flip(hash)
-  flipped = {}
-  hash.each do |letter, number|
-    flipped[number] = letter
+# def flip(hash)
+#   flipped = {}
+#   hash.each do |letter, number|
+#     flipped[number] = letter
+#   end
+#   flipped
+# end
+
+# p flip({"a" => 1, "b" => 2, "c" => 3})
+
+
+# Complete Data 2 ***************
+
+# Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+# For example, given this array of posts (note that the submitted_by is an id number):
+
+# [
+# {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+# ]
+
+# And this array of users:
+
+# [
+# {user_id: 403, name: "Aunty Em"},
+# {user_id: 231, name: "Joelle P."},
+# {user_id: 989, name: "Lyndon Johnson"},
+# {user_id: 111, name: "Patti Q."},
+# ]
+
+# Return the array of posts as follows:
+
+# [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+def users(posts, users)
+  posts.each do |post|
+    users.each do |user|
+      if user[:user_id] == post[:submitted_by]
+        post[:submitted_by] = user[:name]
+      end
+    end
   end
-  flipped
+  posts
 end
 
-p flip({"a" => 1, "b" => 2, "c" => 3})
+p users([
+  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+  ], [
+    {user_id: 403, name: "Aunty Em"},
+    {user_id: 231, name: "Joelle P."},
+    {user_id: 989, name: "Lyndon Johnson"},
+    {user_id: 111, name: "Patti Q."},
+    ])
