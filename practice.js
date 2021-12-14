956e7ea79693910139fbc430b58d3b5e2dbe699e
therobgeorge <georg1rd@gmail.com>
2021-12-13 21:15:24 -0500

    completes popular posts for ruby and js

--- practice.js
+++ practice.js
@@ -828,12 +828,47 @@
 
 // Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
 
-function menu(object) {
-  let total = 0;
-  for (const [item, price] of Object.entries(object)) {
-    total = total + price;
-  }
-  return total;
+// function menu(object) {
+//   let total = 0;
+//   for (const [item, price] of Object.entries(object)) {
+//     total = total + price;
+//   }
+//   return total;
+// }
+
+// console.log(menu({"hot dog": 2, "hamburger": 3, "steak sandwich": 5, "fries": 1, "cole slaw": 1, "soda": 2}));
+
+// Popular Posts *************
+
+// Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+// Input: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+// Output: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// ]
+
+
+function popular(array) {
+  let likes = "likes";
+  let output = [];
+  array.forEach(post => {
+    if (post[likes] >= 1000) {
+      output.push(post);
+    }
+  });
+  return output;
 }
 
-console.log(menu({"hot dog": 2, "hamburger": 3, "steak sandwich": 5, "fries": 1, "cole slaw": 1, "soda": 2}));
+
+console.log(popular([
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}]));
--- practice.rb
+++ practice.rb
@@ -852,16 +852,48 @@
 
 # Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
 
-def menu(hash)
-  total = 0
-  hash.each do |item, price|
-    total = total + price
-    item
+# def menu(hash)
+#   total = 0
+#   hash.each do |item, price|
+#     total = total + price
+#     item
+#   end
+#   total
+# end
+
+
+# p menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+
+# Popular Posts *************
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+
+def popular(array)
+  output = []
+  array.each do |post|
+    if post[:likes] >= 1000
+      output << post
+    end
   end
-  total
+  output
 end
 
-
-p menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
-
-
+p popular([
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+  ])
